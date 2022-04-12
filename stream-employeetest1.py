154b3d60149d765bb37cb6d0b573bbbdb52c4927
Eben26 <100186606+Eben26@users.noreply.github.com>
2022-04-12 11:20:25 +0530

    Update stream-employeetest1.py

--- stream-employeetest1.py
+++ stream-employeetest1.py
@@ -17,15 +17,15 @@ def predict(model, input_df):
 
 def run():
     from PIL import Image
-    image = Image.open('employeeleftimage.jpg')
-    image_office = Image.open('office.jpg')
-    st.image(image,use_column_width=True)
+    #image = Image.open('employeeleftimage.jpg')
+    #image_office = Image.open('office.jpg')
+    #st.image(image,use_column_width=True)
     add_selectbox = st.sidebar.selectbox(
     "How would you like to predict?",
     ("Online", "Batch"))
     st.sidebar.info('This app is created to predict if an employee will leave the company')
     st.sidebar.success('https://www.pycaret.org')
-    st.sidebar.image(image_office)
+    #st.sidebar.image(image_office)
     st.title("Predicting employee leaving")
     
     if add_selectbox == 'Batch':
